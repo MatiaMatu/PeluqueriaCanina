4f85c5d546dc7b229b958cda7e6df0cf6ad9f7c8
matias <matias_maturana2010@hotmail.com>
2024-05-17 17:01:02 -0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+      apiKey: "AIzaSyC0lY0dzh5SuyxY3_e20b8bEZ0WCkDKE2Y",
+     authDomain: "peluqueriacanina-5fb81.firebaseapp.com",
+    projectId: "peluqueriacanina-5fb81",
+    storageBucket: "peluqueriacanina-5fb81.appspot.com",
+    messagingSenderId: "838252232798",
+    appId: "1:838252232798:web:6d9c6320f92f3c1ec6b4cf"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
